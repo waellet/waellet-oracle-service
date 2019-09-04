475c5b49eeba04f061b647532e8bfdfd2befacf6
Milen Radkov <milenradkov@me.com>
2019-09-04 16:04:43 +0300

    implement polling for new oracle queries

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require("express")
 const rest = require('./rest')
 const app = express()
 const config = require('./config/config')
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
 
 let oracleId = "ok" + config.keypair.publicKey.slice(2);
 
@@ -28,30 +30,39 @@ Node.debugSwagger(false)({
         compilerUrl: 'https://compiler.aepps.com',
         nodes: [{name: 'testnet', instance: node}],
         keypair: config.keypair,
-    }).then(async ae =>
+    }).then(async ae => {
         // Get Oracle Object
-        getOrRegisterOracle(ae)(oracleId)
-        // Populate queries
-        // .then(async oracle => {
-        //     await oracle.postQuery("hack.bg")
-        //     await oracle.postQuery("hack2.bg")
-        //     return ae.getOracleObject(oracleId)
-        // })
-            .then(async oracle => {
-                // Answer queries
-                if (oracle.queries.length) {
-                    for (let query of oracle.queries) {
-                        if (query.response === 'or_Xfbg4g==') {
-                            console.log(`Requested Domain: ${decode(query.query)}`)
-                            const response = "yes";
-                            const respondResult = await oracle.respondToQuery(query.id, response)
-                            console.log(respondResult)
-                        }
-                    }
-                }
-            }).catch(err => console.log(err))
-    ).catch(err => console.log(err))
+        setInterval(() => {
 
+          // This is executed after about 20000 milliseconds.
+          getOrRegisterOracle(ae)(oracleId)
+          // Populate queries
+          // .then(async oracle => {
+          //     await oracle.postQuery("hack.bg")
+          //     await oracle.postQuery("hack2.bg")
+          //     return ae.getOracleObject(oracleId)
+          // })
+              .then(async oracle => {
+                  // Answer queries
+                  if (oracle.queries.length) {
+                      let pendingQueries = oracle.queries.filter((i,n) => {
+                        return n.response ==='or_Xfbg4g==';
+                      }).length;
+                      console.log(`\nPending queries: ${pendingQueries}`)
+                      for (let query of oracle.queries) {
+                          if (query.response === 'or_Xfbg4g==') {
+                              console.log(`Requested Domain: ${decode(query.query)}`)
+                              const response = "yes";
+                              const respondResult = await oracle.respondToQuery(query.id, response)
+                              console.log(respondResult)
+                              pendingQueries--
+                          }
+                      }
+                  }
+              }).catch(err => console.log(err))
+          }, 20000)
+      }
+    ).catch(err => console.log(err))
 })
 
 app.get("/", function (req, res) {
