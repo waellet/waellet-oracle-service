5e12147d39f3b382df5aa14c42e9506d2741573b
Milen Radkov <milenradkov@me.com>
2019-09-04 09:37:14 +0300

    oracle registered, extended, receiving queries

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ require = require("esm")(module/* , options */)
 import Ae from '@aeternity/aepp-sdk/es/ae/universal'
 import Oracle from '@aeternity/aepp-sdk/es/oracle/'
 import Tx from '@aeternity/aepp-sdk/es/tx'
+import * as Crypto from '@aeternity/aepp-sdk/es/utils/crypto'
 
 const express = require("express")
 const rest = require('./rest')
 const app = express()
 const config = require('./config/config')
+
+let oracleId = "ok" + config.keypair.publicKey.slice(2);
 
 Ae({
   url: 'https://sdk-testnet.aepps.com',
@@ -17,35 +20,74 @@ Ae({
   keypair: config.keypair,
 }).then(ae => {
 
-  ae.registerOracle("{'city': str}", "{'tmp': num}", { queryFee: 1, ttl:50 }).catch(async err => await err.verifyTx()).then(oracle => {
-    console.log(oracle);
-  })
-  // ae.registerOracle("{'city': str}", "{'tmp': num}", { queryFee: 1, oracleTtlValue: 50, vmVersion: 0, fee: 16752000000000, ttl:50}).catch(async err => await err.verifyTx()).then(oracle => {
-    // console.log(oracle);
-  // })
+  // // Register Waellet Oracle
+  // ae
+  //   .registerOracle("{'domain': str}", "{'txt': str}", { queryFee: 1, ttl:50 })
+  //   .catch(async err => await err.verifyTx())
+  //   .then(oracle => {
+  //     console.log(oracle);
+  //   })
+
+  // Get Oracle Object and answer queries
+  ae
+    .getOracleObject(oracleId)
+    .then(oracleObject => {
+      
+      oracleObject.queries.forEach(query => {
+        
+        ae
+        .getQueryObject(oracleId, query.id)
+        .then(queryObject => {
+          console.log(queryObject);
+          console.log(queryObject.decode(queryObject.query).toString());
+          queryObject.respond("yes");
+        })
+      });
+    })
+
+  // Extend Oracle
+  ae
+    .extendOracleTtl(oracleId, { type: 'delta', value: 500 })
+    .then(result => {
+      console.log(result);
+    })
 })
 
+Ae({
+  url: 'https://sdk-testnet.aepps.com',
+  internalUrl: 'https://sdk-testnet.aepps.com',
+  compilerUrl: 'https://compiler.aepps.com',
+  keypair: config.client,
+}).then(ae => {
+
+  // Post query to oracle
+  ae
+    .postQueryToOracle(oracleId, "hack.bg")
+    .then(result => {
+      console.log(result);
+    })
+})
 
 app.get("/", function(req, res) {
     res.send("Weallet Oracle Service")
 })
 
 app.get("/v1/api/verify/domain/:domain", function(req, res) {
-    const options = {
-        host: 'dns.google',
-        port: 443,
-        path: '/resolve?name='+req.params.domain+'&type=TXT',
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-    };
+  const options = {
+    host: 'dns.google',
+    port: 443,
+    path: '/resolve?name='+req.params.domain+'&type=TXT',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 
-    rest.getJSON(options, (statusCode, result) => {
-        console.log(`onResult: (${statusCode})\n\n${JSON.stringify(result)}`);
-        res.statusCode = statusCode;
-        res.send(result);
-      })
+  rest.getJSON(options, (statusCode, result) => {
+    // console.log(`onResult: (${statusCode})\n\n${JSON.stringify(result)}`);
+    res.statusCode = statusCode;
+    res.send(result);
+  })
 })
 app.listen(3000)
 
